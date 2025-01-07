b12cd6417e44d94dbe4d2731cf025f4d415390a4
Alexander <alexander.bobovnikov.101@gmail.com>
2025-01-08 03:22:26 +0400

    Создал функции (вызова файла, перемешки, подсчета очков, основной функции вызова) + json файл

---
+++ 1. Checking_the_quiz_func/Check_func_main.py
@@ -0,0 +1,44 @@
+import json
+import random   
+
+
+def open_file_quiz():
+    path_to_file = r"C:\Users\Bobosha\Desktop\GitProj\TestingQuizzes\2. Files with Quiz\test_quiz.json"
+    with open(path_to_file, "r", encoding='utf-8') as open_file:
+        return json.load(open_file)
+    
+
+def shuffle_ques_options(correct_file):
+    random.shuffle(correct_file["questions"])
+    for question in correct_file["questions"]:
+        random.shuffle(question["options"])
+
+
+def check_answer(questions, answers):
+    score = 0
+    for question, answer in zip(questions, answers):
+        if question["answer"] == answer:
+            score += question.get("points", 1)
+    return score    
+
+
+def check():
+    quis_list = open_file_quiz()
+    shuffle_ques_options(quis_list)
+
+    print(f"Название квиза: {quis_list["name"]}")
+    user_ans = []
+
+    for question in quis_list["questions"]:
+        print("\n" + question["question"])
+        for x, option in enumerate(question["options"], start=1):
+            print(f"{x}. {option}")
+        answer = input("Твой ответ: ")
+        user_ans.append(question["options"][int(answer) - 1])
+
+    score = check_answer(quis_list["questions"], user_ans)
+    print(f"Ты набрал {score} очков")
+
+if __name__ == "__main__":
+    check()
+    
